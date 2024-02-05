1e343ddcec7c2f070edd4c396197eb60291528ba
KevenAbraham <abraham.keven20@gmail.com>
2024-02-05 14:08:11 -0300

    adding string connection to appsettings.josn

--- LanchesMac/Models/Categoria.cs
+++ LanchesMac/Models/Categoria.cs
@@ -1,6 +1,11 @@
-﻿namespace LanchesMac.Models
+﻿namespace LanchesMac.Models;
+
+public class Categoria
 {
-    public class Categoria
-    {
-    }
+    public int CategoriaId { get; set; }
+    public string CategoriaNome { get; set; }
+    public string Descricao { get; set; }
+
+    public List<Lanche> Lanches { get; set; }
+
 }
